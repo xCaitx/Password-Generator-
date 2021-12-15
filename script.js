c5db0f7e5f7b2ccc5c0ac82f4dd1f9091fe87882
Caitlin Rose <caitlin.rose@mhcarnegie.com>
2021-12-15 21:25:58 +1100

    late commit but got the button working and logic laid out

---
+++ script.js
@@ -0,0 +1,79 @@
+// Caits user prompts
+
+//Array of options for password generator 
+
+var specialCharacters = ["~","!","@","#","$","%","^","&","*",")","("];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+// link to the generate button or is that already done with event listener? 
+
+function generatePassword (){
+var passwordLength = window.prompt ("How many characters would you like your password to be")
+var askspecialCharacters = window.confirm ("Click OK to include any special characters")
+var asknumbers = window.confirm ("Click OK to include numbers")
+var askuppercase = window.confirm ("Click OK to include uppercase letters")
+var asklowercase = window.confirm ("Click OK to include lowercase letters")
+
+
+
+
+//set up empty array, this is where the all the user options will be stored
+var characterSet = []
+
+// empty string for random password characters to be stored 
+var generatedPassword = "";
+
+//prompt for password length and error message
+if (passwordLength < 8 && passwordLength > 128); {
+  window.alert("Invalid response, please choose a number between 8 and 128");
+  passwordLength = window.prompt ("How many characters would you like your password to be")
+}
+// concat each option into characterSet Array
+if (askspecialCharacters === true) {
+  characterSet=characterSet.concat(specialCharacters);
+}
+if (asknumbers === true) {
+  characterSet=characterSet.concat(numbers);
+}
+if (askuppercase === true) {
+  characterSet=characterSet.concat(upperCase);
+}
+if (asklowercase === true) {
+  characterSet=characterSet.concat(lowerCase);
+}
+//enter console logs to check arrays are showing up
+console.log (characterSet);
+
+//set up for loop
+for (var i=0; i <passwordLength; i++) {
+  var index = Math.floor(Math.random() * passwordLength);
+  }
+
+return "password123";
+}
+
+// need to continue the loop and concat the letter to generated password. 
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+//user prompts 
+
+
+//   return generatePassword();
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
